5b56937d741579bce9df4d8b4b21631dd61aedb1
itstsakifs <itstsakif@gmail.com>
2023-05-13 13:42:24 +0700

    first comit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const BP = require('body-parser');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+
+const app =express()
+const port = 3000;
+
+app.use(helmet());
+require("dotenv").config();
+app.use(bodyParser.json());
+app.use(BP.urlencoded({extended: false}));
+app.use(logger('dev'));
+
+app.get('/', (req,res) => {
+    res.send('hlo world')
+})
+
+app.listen(port, ()=>{
+    console.log(`server jalan http://localhost:${port}`)
+});
