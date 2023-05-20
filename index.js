38fdd50dc6e770d0d19464cb6ea26fc411e1940f
itstsakifs <itstsakif@gmail.com>
2023-05-20 09:46:29 +0700

    selesai

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const BP = require('body-parser');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const newsRoute = require('./routes/newsRoute');
-const fileUpload = require("express-fileupload");
+// const verifyToken = require('./middleware/auth');
 
-const app = express()
+const app =express()
 const port = 3000;
 
 
 app.use(helmet());
 require("dotenv").config();
-app.use(express.static("public"));
-app.use(fileUpload());
 app.use(bodyParser.json());
-app.use(BP.urlencoded({extended: true}));
+app.use(BP.urlencoded({extended: false}));
 app.use(logger('dev'));
 
 app.get('/', (req,res) => {
     res.send('hlo world')
-});
-
-app.use('/api', newsRoute);
-
+})
 
 app.use('/user',require(`./api-user/user`));
 app.use('/bcr',require(`./api-user/testbc`));
+
+// app.get('/protek', verifyToken, (req, res) => {
+//     // Dapatkan informasi pengguna dari req.user
+//     const user = req.user;
+//     res.json({ message: 'Protected route', user, tets: "kamu keren" });
+// });
 
 app.listen(port, ()=>{
     console.log(`server jalan http://localhost:${port}`)
